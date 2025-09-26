8e61d186e232aabe760869eb0f083e0d6e199478
Rito Rhymes <83614463+ritorhymes@users.noreply.github.com>
2025-09-26 16:34:13 -0400

    set foundations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // Add SVGR for TSX/JSX importers (import Icon from './icon.svg')
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
